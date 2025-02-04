ec3b13440bf55c1f731f415d81e59078ed827e28
Shunke4444 <jihadtejam867@gmail.com>
2025-02-04 16:47:45 +0800

    added header for landing page

--- LandingPage.php
+++ LandingPage.php
@@ -1,15 +1,53 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome to Mogmog</title>
+    <link rel="stylesheet" href="LandingPage.css">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@3.4.1/dist/tailwind.min.css" rel="stylesheet">
+
+    <script src="https://kit.fontawesome.com/5756001ff6.js" crossorigin="anonymous"></script>
 </head>
+
 <body>
-    <main class="">
-        <h1>Welcome to Mogmog</h1>
-        <p>Book your next meal with us!</p>
-        <a href="BookingPage.php">Book Now</a>
-    </main>
+
+        <header>
+            <nav class="left-side">
+                <div class="left-buttons">
+                    <button>
+                        <i class="fa-solid fa-plane"></i>
+                        <a href="LandingPage.php">Find Flight</a>
+                    </button>
+
+                    <button>
+                        <i class="fa-solid fa-bed"></i>
+                        <a href="LandingPage.php">Find Stays</a>
+                    </button>
+                </div>
+            </nav>
+
+            <nav class="center">
+                <img src="/assets/GlobeLogo.png" class="logo" alt="Globe Logo">
+            </nav>
+
+            <nav class="right-side">
+                <button>
+                    <i class="fa-solid fa-heart"></i>
+                    <a href="LandingPage.php">Favourites</a>
+                </button>
+                    <p>|</p>
+
+                <button>
+                <a href="LandingPage.php">User Profile</a>
+                </button>
+            </nav>
+        </header>
+
+        <main>
+            <img src="" alt="">
+        </main>
 </body>
+
 </html>
